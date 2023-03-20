04440c43411102fb0ae5cfb7f8cea47349f919a5
lvhao09 <lvhao09@meituan.com>
2023-03-20 15:15:16 +0800

    搭建payment服务骨架

---
+++ cloud-provider-payment8001/src/main/java/com/lvhao/springcloud/PaymentApp8001.java
@@ -0,0 +1,17 @@
+package com.lvhao.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lvhao09
+ * @date 2023/3/20
+ */
+@SpringBootApplication
+public class PaymentApp8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApp8001.class, args);
+    }
+
+}
